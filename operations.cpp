b5afecd0ea4a431fdb1b813512fd31e0f87e2269
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-09-20 19:30:19 +0700

    Fix mistakes

--- operations.cpp
+++ operations.cpp
@@ -3,16 +3,16 @@
 int *getRight(int *start)
 {
     int *arr = new int[2];
-    arr[0] = start[0] + 1;
-    arr[1] = start[1];
+    arr[0] = start[0];
+    arr[1] = start[1] + 1;
     return arr;
 }
 
 int *getLeft(int *start)
 {
     int *arr = new int[2];
-    arr[0] = start[0] - 1;
-    arr[1] = start[1];
+    arr[0] = start[0];
+    arr[1] = start[1] - 1;
     return arr;
 }
 
